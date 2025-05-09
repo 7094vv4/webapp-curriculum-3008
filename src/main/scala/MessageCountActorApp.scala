841e205d7cebbcf4021ebde3cf4f379757fc86d6
7094vv4 <24241035+7094vv4@users.noreply.github.com>
2025-05-09 22:50:24 +0900

    初級の回答

--- src/main/scala/MessageCountActorApp.scala
+++ src/main/scala/MessageCountActorApp.scala
@@ -1,3 +1,29 @@
-object MessageCountActorApp extends App {
+import akka.actor.{Actor, ActorSystem, Props}
+import scala.concurrent.Await
+import scala.concurrent.duration._
 
+class MessageCountActor extends Actor {
+  var count = 0
+
+  def receive: Receive = {
+    case n: Int =>
+      count += 1
+      println(s"Count: $n")
+      if (n >= 10000) {
+        println("Finished counting. Shutting down system.")
+        context.system.terminate()
+      }
+  }
 }
+
+object MessageCountActorApp extends App {
+  val system = ActorSystem("MessageCountSystem")
+  val counter = system.actorOf(Props[MessageCountActor], "counterActor")
+
+  for (i <- 1 to 10000) {
+    counter ! i
+  }
+
+  Await.result(system.whenTerminated, Duration.Inf)
+}
+
